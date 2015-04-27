7bf6e8f66a6cc23ed713ed594daa78bd3d48b3eb
Bernard Banta <banta.bernard@gmail.com>
2015-04-27 16:41:20 +0300

    Updated deploy file

--- config/deploy.rb
+++ config/deploy.rb
@@ -54,3 +54,21 @@ namespace :deploy do
   end
 
 end
+
+before 'deploy:update', 'deploy:update_jekyll'
+
+namespace :deploy do
+  [:start, :stop, :restart, :finalize_update].each do |t|
+    desc "#{t} task is a no-op with jekyll"
+    task t, :roles => :app do ; end
+  end
+
+  desc 'Run jekyll to update site before uploading'
+  task :update_jekyll do
+    # clear existing _site
+    # build site using jekyll
+    # remove Capistrano stuff from build
+    %x(rm -rf _site/* && jekyll build && rm _site/Capfile && rm -rf _site/config)
+  end
+end
+
