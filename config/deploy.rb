ffa90054003bb8b26b3a78dab62c7efaae530360
Bernard Banta <banta.bernard@gmail.com>
2015-07-15 02:49:52 +0300

    Remove the config dir and Capfile, and run jekyll build

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,14 +4,8 @@ lock '3.4.0'
 set :application, 'nairuby.org'
 set :repo_url, 'git@github.com:nairuby/nairuby.github.io.git'
 
-namespace :deploy do
-
-  after :restart, :clear_cache do
-    on roles(:web), in : :groups, limit : 3, wait : 10 do
-    # Here we can do anything such as:
-    # within release_path do
-    #   execute :rake, 'cache:clear'
-    # end
-    end
-  end
+# Remove the config dir and Capfile, and run jekyll build
+after "deploy:symlink:shared" do
+  run "rm -rf #{release_path}/config #{release_path}/Capfile"
+  run "cd #{release_path} && jekyll build"
 end
