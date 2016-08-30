5e9736c4efb9be61ad8ded8ba4e99bd4872f58ff
banta <banta.bernard@gmail.com>
2016-08-31 01:11:34 +0300

    Changed server address

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
 set :deploy_to, '/var/www/nairuby.org'
 set :branch, 'master'
 
-role :app, %w{ubuntu@128.199.166.194}
-role :web, %w{ubuntu@128.199.166.194}
-role :db,  %w{ubuntu@128.199.166.194}
+role :app, %w{ubuntu@192.241.248.97}
+role :web, %w{ubuntu@192.241.248.97}
+role :db,  %w{ubuntu@192.241.248.97}
