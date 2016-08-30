e374588dfe029bbdaece925778526caa048c4528
banta <banta.bernard@gmail.com>
2016-08-31 01:11:57 +0300

    Changed ruby and gemset version

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,4 +4,4 @@ lock '3.4.0'
 set :application, 'nairuby.org'
 set :repo_url, 'git@github.com:banta/nairuby.github.io.git'
 set :log_level, :debug
-set :rvm_ruby_version, 'ruby-2.2.1@rails4'
+set :rvm_ruby_version, '2.3.0@rails'
