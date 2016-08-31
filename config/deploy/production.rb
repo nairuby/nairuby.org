272035f5b3c06b114a61dae7667cfd03c8603712
banta <banta.bernard@gmail.com>
2016-08-31 12:39:12 +0300

    Changed deployment server IP

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
 set :deploy_to, '/var/www/nairuby.org'
 set :branch, 'master'
 
-role :app, %w{ubuntu@192.241.248.97}
-role :web, %w{ubuntu@192.241.248.97}
-role :db,  %w{ubuntu@192.241.248.97}
+role :app, %w{ubuntu@52.36.134.184}
+role :web, %w{ubuntu@52.36.134.184}
+role :db,  %w{ubuntu@52.36.134.184}
