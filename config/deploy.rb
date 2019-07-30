7c97f29e176660e530c8b6953f073d2532440a25
banta <banta.bernard@gmail.com>
2019-07-30 14:13:50 +0300

    Change deployment repo url

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,6 +2,6 @@
 lock '3.4.1'
 
 set :application, 'nairuby.org'
-set :repo_url, 'git@github.com:banta/nairuby.github.io.git'
+set :repo_url, 'git@github.com:nairuby/nairuby.github.io.git'
 set :log_level, :debug
 set :rvm_ruby_version, '2.3.0@rails'
